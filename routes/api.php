192b113593d7c1baefc1912d71df83858d6054b6
gabriel.rolim1@aluno.ce.gov.br <amanda.eduardo@aluno.ce.gov.br>
2025-06-05 16:23:25 -0300

    finalizei api 2

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,4 @@ use App\Http\Controllers\Api\ArtworkController;
 
 Route::get('/artworks', [ArtworkController::class, 'index']);
 Route::post('/artworks', [ArtworkController::class, 'store']);
+//só atualizando 
